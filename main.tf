1965ebbf48a1abb36ad6354930d04671b3ce6ced
HashiCorp Training <training@hashicorp.com>
2017-01-27 17:55:55 +0000

    Initial commit.

---
+++ main.tf
@@ -0,0 +1,70 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your subnet ID is:
+#
+#     subnet-ddd57685
+#
+# Your security group ID is:
+#
+#     sg-29ef374e
+#
+# Your AMI ID is:
+#
+#     ami-30217250
+#
+# Your Identity is:
+#
+#     autodesk-crab
+#
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+
+  secret_key = "${var.aws_secret_key}"
+
+  region = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-30217250"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-ddd57685"
+  count         = "${var.num_webs}"
+
+  vpc_security_group_ids = [
+    "sg-29ef374e",
+  ]
+
+  tags {
+    Identity = "autodesk-crab"
+    Name     = "fumi ${count.index+1}/${var.num_webs}"
+    Group    = "search"
+  }
+}
+
+variable "aws_access_key" {
+  type = "string"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+}
+
+variable "aws_region" {
+  type    = "string"
+  default = "us-west-1"
+}
+
+variable "num_webs" {
+  type    = "string"
+  default = "2"
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
